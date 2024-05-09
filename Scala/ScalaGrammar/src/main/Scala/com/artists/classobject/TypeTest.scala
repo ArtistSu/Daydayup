e2aee2ad79833cc7aedb501a9a24d575f6bb85a8
ArtistS <sylgg0918@gmail.com>
2024-05-09 16:09:15 +0100

    type study

---
+++ Scala/ScalaGrammar/src/main/Scala/com/artists/classobject/TypeTest.scala
@@ -0,0 +1,13 @@
+package main.Scala.com.artists.classobject
+
+/**
+ * 1. type can set a alias for a complex type name
+ */
+class TypeTest {
+
+    type alias_type = (Int,String,Int,String,String)
+    val tmp:alias_type = (1,"2",3,"4","5")
+
+    type myListtype = scala.collection.immutable.List[Int]
+    val list:myListtype = List[myListtype](1,2,3,4,5,6)
+}

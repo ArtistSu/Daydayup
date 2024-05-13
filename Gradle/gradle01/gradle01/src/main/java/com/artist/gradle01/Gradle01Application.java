0ab08d468594939ef8aeb9bdd376cef5062594a7
ArtistS-win <sylgg0918@gmail.com>
2024-05-13 21:43:39 +0100

    gradle create project

---
+++ Gradle/gradle01/gradle01/src/main/java/com/artist/gradle01/Gradle01Application.java
@@ -0,0 +1,13 @@
+package com.artist.gradle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradle01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gradle01Application.class, args);
+	}
+
+}
---
+++ Gradle/gradle01/gradle01/src/test/java/com/artist/gradle01/Gradle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artist.gradle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradle01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

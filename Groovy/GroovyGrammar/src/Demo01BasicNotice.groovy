6cbd195e5ee5e25e18ccd9ede2900ce7b5546884
ArtistS-win <sylgg0918@gmail.com>
2024-05-16 23:03:58 +0100

    Groovy basic notice

---
+++ Groovy/GroovyGrammar/src/Demo01BasicNotice.groovy
@@ -0,0 +1,27 @@
+/**
+ * 1. Groovy use def to define properties, and methods. def
+ * support dynamic type declaration.
+ * 2. Single line comments -> //
+ * Multi-line comments -> /**
+ * 3. The ; at the end of Groovy statement can be omitted.
+ * 4. Groovy can generate getter() and setter() for properties
+ * automatically.
+ * 5. Method Declaration: parameters type, return type, and
+ * return keyword can be omitted. The () can be omitted when
+ * call a method, if it is unambiguous.
+ * 6. Variable reference: {} can also be omitted if it is
+ * unambiguous.
+ * 7. Write properties:
+ *  1. stu.name =
+ *  2. stu["name"] =
+ *  3. stu.setName()
+ *  4. constructor
+ *
+ *  Read properties:
+ *  1. stu.name
+ *  2. stu["name"]
+ *  3. stu.getName()
+ */
+class Demo01BasicNotice {
+
+}

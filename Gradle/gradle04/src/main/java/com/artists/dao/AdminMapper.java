89309f44a7c0b54f9626923f958dbc039ba06275
ArtistS-win <sylgg0918@gmail.com>
2024-05-24 09:35:11 +0100

    Gradle Test

--- Gradle/gradle04/src/main/java/com/artists/dao/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package com.artists.dao;
-
-import com.artists.bean.Admin;
-
-import java.util.List;
-
-public interface AdminMapper {
-    List<Admin> getAdminList();
-}
---
+++ Gradle/gradle04/src/test/java/com/artists/test/AppTest.java
@@ -0,0 +1,11 @@
+package com.artists.test;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testMethod_1(){
+        System.out.println("Test artists gradle test");
+    }
+}

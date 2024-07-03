ea1db5d6ab7306b321b6aabb7b3a3ce2d84c1801
ArtistS-win <sylgg0918@gmail.com>
2024-07-03 22:20:16 +0100

    fix conflict

--- Algorithm/src/main/java/com/artists/sorting/heap/HeapSort.java
+++ Algorithm/src/main/java/com/artists/sorting/heap/HeapSort.java
@@ -7,8 +7,6 @@ import com.artists.common.Xor;
  * Space Complexity: O(1)
  */
 public class HeapSort {
-<<<<<<< HEAD
-
     public static void heapSort(int[] arr){
         if(arr == null ||arr.length < 2){
             return;
@@ -21,16 +19,6 @@ public class HeapSort {
         while(heapSize > 0){
             heapify(arr,0,heapSize);
             Xor.swap(arr,0,--heapSize);
-=======
-    public void heapSort(int[] arr){
-        for (int i = arr.length-1/2; i >=0 ; i--) {
-            heapify(arr,arr.length,i);
-        }
-
-        for (int i = arr.length-1; i > 0 ; i--) {
-            Xor.swap(arr,0,i);
-            heapify(arr,0,i);
->>>>>>> f6a59244eb4b8cc695c48535f5c8354454296a55
         }
     }
     public static void heapify(int[] arr, int index, int heapSize) {
